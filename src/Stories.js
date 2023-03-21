b01d089dac8c882547ff8ea1193e7d543a6145c5
eleftheriasavvidou <32686987+eleftheriasavvidou@users.noreply.github.com>
2023-03-21 14:33:41 +0200

    Upload project

---
+++ src/Stories.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { useFetch } from './hooks';
+
+function Stories() {
+
+    const stories = useFetch('https://news-proxy-230704.appspot.com/topstories', [])
+
+    return (
+        <div className='Stories' >
+            <h3>Stories</h3>
+            {
+                stories.map(story => {
+                    const { id, by, time, title, url } = story;
+
+                    // API returns a time in UNIX time (JS assumes milliseconds). We multiply by 1000 to get the correct date
+                    return (
+                        <div key={id}>
+                            <a href={url}>{title}</a>
+                            <div>{by} - {new Date(time * 1000).toLocaleString()} </div>
+                        </div> 
+                    )
+                })
+            }
+
+        </div>
+    )
+
+}
+
+export default Stories
