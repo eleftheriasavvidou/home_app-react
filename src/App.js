fd6fba2791bad27dbf8dc25cb77d4d6a114c24ed
eleftheriasavvidou <32686987+eleftheriasavvidou@users.noreply.github.com>
2023-03-22 16:18:49 +0200

    Update Project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Joke from "./Joke";
 import Stories from "./Stories";
+import Tasks from "./Tasks";
 
 function App() {
 
@@ -33,6 +34,8 @@ function App() {
       </div>
       <hr />
       <Joke />
+      <hr />
+      <Tasks />
       <hr />
       <Stories />
 
